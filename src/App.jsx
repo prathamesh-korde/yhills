b0e1c0e235dd11cb3e0feab67bc2bec276bced5b
prathamesh-korde <kordeprathamesh01@gmail.com>
2025-03-20 23:34:56 +0530

    props drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,73 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import Home from './Home';
-import Exp from './exp';
-import Profile from './profile';
+import { useState } from "react";
+import "./App.css";
+import Exp from "./exp";
+import Profile from "./profile";
+
 function App() {
   const userlogin = true;
-  const [state,setState]=useState(1);
+  const [state, setStp] = useState("+917825486975");
 
-  function handelSubmit(){
-    const input = document.getElementById('input').value;
-    alert(input);
-  }
-  function inc(){
-   setState(state*2)
+  {/*
+  function handleSubmit() {
+    alert(`Step: ${step}, Name: ${name}`);
   }
 
-  function dec(){
-    setState(state/2)
+  function inc() {
+    if (!isNaN(step) && step !== 0) {
+      setState(state * step);
+    }
   }
-  
+
+  function dec() {
+    if (step !== 0) {
+      setState(state / step);
+    } else {
+      alert("Step value cannot be zero.");
+    }
+  }
+  */}
+
   return (
     <>
     <p>{state}</p>
-   <button onClick={inc}>increase</button>
-   <br/>
-   <br/>
-   <button onClick={dec}>decrease</button>
-   <br/>
-   <br/>
-   <input type="text" placeholder='enter your name'  className='main_input' id="input"/>
-   <br/>
-   <button className='main_input' onClick={handelSubmit}>Submit</button>
-    
-      {/*<Home name="portfolio"/>
-      <Exp name ="prathamesh"></Exp>*/}
-      {
-      userlogin == true ? <Profile></Profile>  :<p>plese login !</p>
-}
+      <Exp state={state}  setStp ={setStp}/>
+      <br />
+{/*
+      <input
+        type="number"
+        placeholder="Enter step value"
+        className="stp"
+        value={step}
+        onChange={(e) => setStep(Number(e.target.value) || 1)}
+      />
+      <br />
+
+      <p>{state}</p>
+
+      <button onClick={inc}>Increase</button>
+      <br />
+      <br />
+      <button onClick={dec}>Decrease</button>
+      <br />
+      <br />
+
+      <input
+        type="text"
+        placeholder="Enter your name"
+        className="main_input"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      <button className="main_input" onClick={handleSubmit}>
+        Submit
+      </button>
+
+      {/* Conditional Rendering 
+      {userlogin ? <Profile /> : <p>Please login!</p>}
+      */}
     </>
-  );  
+  );
 }
 
-export default App
+export default App;
--- src/Exp.jsx
+++ src/Exp.jsx
@@ -1,38 +1,33 @@
 import React from "react";
-import './App.css';
-import data from './data.json';
+import "./App.css";
+import data from "./data.json";
 
-const Exp = (props) =>{
+const Exp = (props) => {
+  let {state,setStp}= props;
 
-    const person = {
-        name:"pratham",
-        age:21
-    }
-    const stu = [1,2,3,4,5,9,10]
-    //const {name} = props;
-    return(
-        <>
-        <h1>{person.name}</h1>
-        
-       {/* {
-            stu.map((itm,index)=>
-                <p key={index}>{itm}</p>
-            )
-        }
-            */}
-            {
-                data.map((item,index)=>{
-                    return(
-                    <div key={index}>  
-                        <h1>{item.name}</h1>
-                        <p>{item.email}</p>
-                        <p>{item.age}</p>
-                    </div>
-                )}
-            )
-            }
-        </>
-    )
-}
+  function handelChange(e){
+    setStp(e.target.value)
+  }
+  return (
+    <>
+      <h2>Phone No: {state}</h2>
+
+<input name="phone" onChange={handelChange}></input>
+{/*
+      {data.length > 0 ? (
+        data.map((item, index) => (
+          <div key={index}>
+            <h1>{item.name}</h1>
+            <p>{item.email}</p>
+            <p>{item.age}</p>
+          </div>
+        ))
+      ) : (
+        <p>No data available</p>
+      )}
+        */}
+    </>
+  );
+};
 
 export default Exp;
